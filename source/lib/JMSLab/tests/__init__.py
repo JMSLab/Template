9741b5959bccc039507e33c8371c752c93ca280b
Mauricio Caceres <mauricio.caceres.bravo@gmail.com>
2021-01-07 22:35:31 -0500

    #4 Commented out tablefill tests

--- source/lib/JMSLab/tests/__init__.py
+++ source/lib/JMSLab/tests/__init__.py
@@ -4,6 +4,9 @@ This directory contains the JMSLab SCons' library's unit tests. These
 are modified versions of the unit tests in gslab_scons.  Run via
 
     export JMSLAB_EXE_STATA="/path/to/stata"
+    python -m unittest discover
+
+Individual tests can be run via
 
     python -m unittest JMSLab.tests.test_build_latex
     python -m unittest JMSLab.tests.test_build_lyx
@@ -13,4 +16,5 @@ are modified versions of the unit tests in gslab_scons.  Run via
     python -m unittest JMSLab.tests.test_build_stata
     python -m unittest JMSLab.tests.test_build_log
     python -m unittest JMSLab.tests.test_build_misc
+    # python -m unittest JMSLab.tests.test_build_tables
 '''
