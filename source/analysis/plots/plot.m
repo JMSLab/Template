993dd314139c42e4a07253eb58b7237e311fe564
Mauricio Caceres Bravo <mauricio.caceres.bravo@gmail.com>
2021-01-10 23:32:42 -0500

    Pull Request for #11: Use built-in path joins (#13)
    * #11 Use built-in utilities for path joins
    
    * #11 Moved to forward slashes for file paths
    
    * Minor style update to tot_gdp
    
    * Proposed change for wb_clean
    
    * Delete old unwanted output
    
    Co-authored-by: santiagohermo <santiago_hermo@brown.edu>

--- source/analysis/plots/plot.m
+++ source/analysis/plots/plot.m
@@ -1,4 +1,5 @@
-dataset = readtable('.\output\derived\wb_clean\gdp_education_logs.csv');
+dataset = readtable(fullfile('output/derived/wb_clean', 'gdp_education_logs.csv'));
+outstub = fullfile('output/analysis/plots', 'gdp_educ');
 
 x = dataset{:,'log_education_exp_2010'};
 y = dataset{:,'log_gdp_2010'};
@@ -7,8 +8,8 @@ scatter(x,y);
 set(gca,'FontName','Times New Roman');
 xlabel('Log of Total Government Expenditure on Education in 2010 (% of GDP)', 'FontSize', 10);
 ylabel('Log of GDP per capita in 2010 (current US$)', 'FontSize', 10);
-print ('-depsc','-painters', '.\output\analysis\plots\gdp_educ.eps');
-print ('-dpng','-painters', '.\output\analysis\plots\gdp_educ.png');
+print('-depsc','-painters', [outstub '.eps']);
+print('-dpng','-painters', [outstub '.png']);
 close;
 
 exit
