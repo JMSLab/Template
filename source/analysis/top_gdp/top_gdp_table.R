48e702f9997116ee2471d6e46a26a5ca02d0a558
santiagohermo <santiago_hermo@brown.edu>
2021-01-10 21:05:04 -0500

    Minor style update to tot_gdp

--- source/analysis/top_gdp/top_gdp_table.R
+++ source/analysis/top_gdp/top_gdp_table.R
@@ -1,22 +1,23 @@
 library(dplyr)
 
 Main <- function() {
-  
-  df <- read.csv(file.path("output/derived/wb_clean", "gdp_education.csv"))
+  instub  <- "output/derived/wb_clean"
+  outstub <- "output/analysis/top_gdp"
+
+  df <- read.csv(file.path(instub, "gdp_education.csv"))
   
   df <- df %>%
     select(Country.Name, GDP_2010) %>%
     arrange(-GDP_2010) %>% top_n(5)
   
   tag <- "<tab:top_gdp>"
-  out <- file.path("output/analysis/top_gdp", "top_gdp.txt")
+  out <- file.path(outstub, "top_gdp.txt")
   
   write.table(tag, row.names = FALSE, quote = FALSE,
               file = out, col.names = FALSE)
   
   write.table(df, row.names = FALSE, quote = FALSE,
               file = out, append = TRUE, col.names = FALSE, sep = "\t")
-  
 }
 
 Main()
