3a8c3fc5ef4ebe3877b45fc2aa119666ccac195d
Jin <jin.fu.93@gmail.com>
2022-03-24 17:12:13 -0400

    #51 Relocates fill targets

--- source/lib/JMSLab/tests/test_autofill.py
+++ source/lib/JMSLab/tests/test_autofill.py
@@ -6,15 +6,16 @@ from unittest import main, TestCase
 from JMSLab.autofill import GenerateAutofillMacros
 from os.path import exists
 
-P_94 = 16.22
-Epsilon = - 1.19
-MarginalCost = (1 + 1 / Epsilon) * P_94
-SurplusGain = P_94 - MarginalCost
 
-autofill_outfile = r"temp\output_macros.tex"
 
 class Test(TestCase):
     def test_file_exists(self):
+        
+        P_94 = 16.22
+        Epsilon = - 1.19
+        MarginalCost = (1 + 1 / Epsilon) * P_94
+        autofill_outfile = r"temp\output_macros.tex"
+        
         GenerateAutofillMacros(["Epsilon", "MarginalCost"], "{:.2f}", autofill_outfile)
         self.assertTrue(exists(autofill_outfile))
         return
