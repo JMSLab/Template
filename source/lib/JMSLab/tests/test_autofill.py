20ad57a41db1979007c9b63ea37685b2986bcfc7
Santiago Hermo <45404755+santiagohermo@users.noreply.github.com>
2022-04-15 11:19:47 -0400

    Drop unnecessary blank spaces #55 #57

--- source/lib/JMSLab/tests/test_autofill.py
+++ source/lib/JMSLab/tests/test_autofill.py
@@ -5,54 +5,54 @@ from os.path import exists
 from ..autofill import GenerateAutofillMacros
 
 class Test(TestCase):
-    
+
     def setUp(self):
         self.tempdir = tempfile.mkdtemp() 
         self.outfile = self.tempdir + r"output_macros.tex"
         return
-    
+
     def tearDown(self):
         shutil.rmtree(self.tempdir)
         return
-    
+
     def test_file_exists(self):
         Epsilon = -1.19
         
         GenerateAutofillMacros(["Epsilon"], self.outfile)
         self.assertTrue(exists(self.outfile))
         return
-    
+
     def test_exception(self):
         with self.assertRaises(Exception) as context:
             GenerateAutofillMacros(["Epsilon"], self.outfile)
 
         self.assertTrue("Autofill: Variable 'Epsilon' not found" in str(context.exception))
         return
-    
+
     def test_output(self):
         Epsilon = - 1.19
-       
+        
         GenerateAutofillMacros(["Epsilon"], self.outfile)
         tex_file = open(self.outfile, 'r')
         content = tex_file.read()
         self.assertEqual(content, "\\newcommand{\\Epsilon}{-1.19}\n")
         tex_file.close()
         return
-    
+
     def test_list_format(self):
         Epsilon = - 1.19
         MarginalCost = 2.59
-            
+
         with self.assertRaises(Exception) as context:
             GenerateAutofillMacros("Epsilon", self.outfile)
 
         self.assertTrue("Argument 'autofill_lists' must be list" in str(context.exception))
-            
+
         with self.assertRaises(Exception) as context:
             GenerateAutofillMacros(["Epsilon"], self.outfile, ["{:.2f}", "\\textnormal{{{:.2f}}}"])
 
         self.assertTrue("Arguments 'autofill_lists' and 'autofill_formats' are incompatible" in str(context.exception))
-            
+
         with self.assertRaises(Exception) as context:
             GenerateAutofillMacros([["Epsilon"], ["Marginal Cost"]], self.outfile)
 
