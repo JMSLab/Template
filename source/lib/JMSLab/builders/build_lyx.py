e1ebb0f9753963d281d92c4cd1e0203fcc575931
Richard Calvo <97416734+rcalvo12@users.noreply.github.com>
2022-05-12 10:37:19 -0400

    #49 Produces handout from talk_slide.pdf

--- source/lib/JMSLab/builders/build_lyx.py
+++ source/lib/JMSLab/builders/build_lyx.py
@@ -1,6 +1,9 @@
 import subprocess
 import os
+import shutil
+import fileinput
 
+from .. import misc
 from .jmslab_builder import JMSLabBuilder
 
 
@@ -26,6 +29,7 @@ def build_lyx(target, source, env):
         'exec_opts': '-E pdf2'
     }
     builder = LyxBuilder(target, source, env, **builder_attributes)
+    builder.create_handout()
     builder.execute_system_call()
     return None
 
@@ -42,3 +46,54 @@ class LyxBuilder(JMSLabBuilder):
                                   os.path.normpath(self.log_file))
         self.call_args = args
         return None
+
+    def create_handout(self):
+        '''
+        Converts notes to greyedout for export to handout
+        '''
+
+        source_name = os.path.splitext(str(self.source_file))[0]
+        target_name = os.path.splitext(str(self.target[0]))[0]
+        
+        handout_doc_suffix = '_handout'
+        handout_doc_input  = source_name + handout_doc_suffix + '.lyx'
+        handout_doc_output = target_name + handout_doc_suffix + '.pdf'
+
+        shutil.copy2(self.source_file, handout_doc_input)
+        beamer = False
+        for line in fileinput.input(handout_doc_input, inplace = True):
+            if r'\textclass beamer' in line:
+                beamer = True
+            elif r'\begin_inset Note Note' in line and beamer:
+                line = line.replace('Note Note', 'Note Greyedout')
+            print(line)
+        
+        args = '%s %s %s > %s' % (handout_doc_output,
+                               handout_doc_input,
+                                self.cl_arg,
+                               os.path.normpath(self.log_file))
+        
+        self.handout_args = args
+
+        self.handout_call = '%s %s %s' % (self.executable, self.exec_opts, self.handout_args)
+        subprocess.check_output(self.handout_call, shell = True, stderr = subprocess.STDOUT)
+
+
+def do_call(self):
+    '''
+    Acutally execute the system call attribute.
+    Raise an informative exception on error.
+    '''
+    
+    traceback = ''
+    raise_system_call_exception = False
+    try:
+        subprocess.check_output(self.system_call, shell = True, stderr = subprocess.STDOUT)
+    except subprocess.CalledProcessError as ex:
+        traceback = ex.output
+        raise_system_call_exception = True
+
+    self.cleanup()
+    if raise_system_call_exception:
+        self.raise_system_call_exception(traceback = traceback)
+    return None
