9a49268f3b8dffb8a48f2d0b843c0912c0fa5355
Richard Calvo <97416734+rcalvo12@users.noreply.github.com>
2022-07-03 17:25:33 -0400

    #56 #42 Fixing how we switch to bib side_effect

--- source/lib/JMSLab/tests/_side_effects.py
+++ source/lib/JMSLab/tests/_side_effects.py
@@ -136,13 +136,6 @@ def lyx_side_effect(*args, **kwargs):
     and can export .lyx files to .pdf files only using
     the "-e pdf2" option.
     '''
-    
-    bibmatch = helpers.command_match(command, 'bibtex')
-    if bibmatch:
-        bibtex_side_effect(*args, **kwargs)
-        return
-
-    match = helpers.command_match(command, 'pdflatex')
 
     # Get and parse the command passed to os.system()
     command = args[0]
@@ -190,8 +183,15 @@ def latex_side_effect(*args, **kwargs):
     The mocked machine has pdflatex set up as a command-line executable
     and can export .tex files to .pdf files only using the "-jobname" option.
     '''
+    
     # Get and parse the command passed to os.system()
     command = args[0]
+    
+    bibmatch = helpers.command_match(command, 'bibtex')
+    if bibmatch:
+        bibtex_side_effect(*args, **kwargs)
+        return
+
     match = helpers.command_match(command, 'pdflatex')
 
     executable   = match.group('executable')
