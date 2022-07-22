1e9bbc522bc0d122dff5646cef09afdec48ce435
ew487 <108754119+ew487@users.noreply.github.com>
2022-07-22 17:20:56 -0400

    #49 Implement iteration with HANDOUT_SFIX controlling behavior
    In this iteration, we operate on the implicit assumption that the target[0] is the path at which the clean pdf is to be created. A construction string variable called HANDOUT_SFIX controls whether or not the handout pdf is generated. When HANDOUT_SFIX is set to empty (default), only the clean pdf is generated (no handout). When HANDOUT_SFIX is not empty, we check whether any paths in the target list have a tail that matches {the tail of target[0] with HANDOUT_SFIX inserted immediately before the file extension}. If check returns true, we generate handout pdfs at matching paths (as well as clean pdf at target[0]). If check returns false, we raise an exception.

--- source/lib/JMSLab/builders/build_lyx.py
+++ source/lib/JMSLab/builders/build_lyx.py
@@ -48,13 +48,10 @@ class LyxBuilder(JMSLabBuilder):
         '''
         Converts notes to greyedout for export to handout
         '''
-        file = str((misc.make_list_if_string(target))[0])
-        file_root = os.path.splitext(file)[0]
-        file_ext = os.path.splitext(file)[1]
-        handout_suffix = "_handout"
+        file_root, file_ext = os.path.splitext(str(self.pdf_target))
         
-        self.handout_doc_output =  file_root + handout_suffix + file_ext
-        self.handout_doc_input  = file_root + handout_suffix + '.lyx'
+        self.handout_doc_output =  file_root + env['HANDOUT_SFIX'] + file_ext
+        self.handout_doc_input  = file_root + env['HANDOUT_SFIX'] + '.lyx'
 
         shutil.copy2(self.source_file, self.handout_doc_input)
         beamer = False
@@ -77,8 +74,13 @@ class LyxBuilder(JMSLabBuilder):
 
 
     def cleanup_handout(self):
+        
+        handout_target_dest = [x for x in self.handout_target if self.handout_doc_output != str(x)]
+        for x in handout_target_dest:
+            shutil.copy2(self.handout_doc_output, str(x))
+        if set(self.handout_target) == set(handout_target_dest):
+            os.remove(self.handout_doc_output)
         os.remove(self.handout_doc_input)
-        shutil.move(self.handout_doc_output, 'temp/' + os.path.split(self.handout_doc_output)[1])
         return None
 
 
@@ -87,19 +89,36 @@ class LyxBuilder(JMSLabBuilder):
         Acutally execute the system call attribute.
         Raise an informative exception on error.
         '''
-        if bool(env['HANDOUT_BOOL']):
-            self.create_handout(target, env)
-            traceback = ''
-            raise_system_call_exception = False
-            try:
-                subprocess.check_output(self.handout_call, shell = True, stderr = subprocess.STDOUT)
-            except subprocess.CalledProcessError as ex:
-                traceback = ex.output
-                raise_system_call_exception = True
-
-            self.cleanup_handout()
-            if raise_system_call_exception:
-                self.raise_system_call_exception(traceback = traceback)
+        #'''
+        if bool(env['HANDOUT_SFIX']):
+            
+            pdf_target = misc.make_list_if_string(target)[0]
+            pdf_target_tail = os.path.split(str(pdf_target))[1]
+            pdf_target_tail_root, pdf_target_tail_ext = os.path.splitext(pdf_target_tail)
+            handout_target_check = pdf_target_tail_root + env['HANDOUT_SFIX'] + pdf_target_tail_ext
+            
+            handout_target = [x for x in misc.make_list_if_string(target) 
+                              if os.path.split(str(x))[1] == handout_target_check]
+            
+            if bool(handout_target):
+                self.pdf_target, self.handout_target = pdf_target, handout_target
+                self.create_handout(target, env)
+                
+                traceback = ''
+                raise_system_call_exception = False
+                try:
+                    subprocess.check_output(self.handout_call, shell = True, stderr = subprocess.STDOUT)
+                except subprocess.CalledProcessError as ex:
+                    traceback = ex.output
+                    raise_system_call_exception = True
+                    
+                self.cleanup_handout()
+                if raise_system_call_exception:
+                    self.raise_system_call_exception(traceback = traceback)
+                
+            else:
+                raise Exception('The handout_target is empty')
+            
         else:
             pass
 
