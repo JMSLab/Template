3cc76c02c199d3e08650563a2b0173ead79ef859
Chris Liao <33707455+liaochris@users.noreply.github.com>
2024-10-23 09:58:42 -0400

    #107 Update path to be platform agnostic

--- source/lib/JMSLab/SaveData.py
+++ source/lib/JMSLab/SaveData.py
@@ -21,7 +21,7 @@ def SaveData(df, keys, out_file, log_file = '', append = False, sortbykey = True
 def CheckExtension(out_file):
     if type(out_file) == str:
         extension = re.findall(r'\.[a-z]+$', out_file)
-    elif type(out_file) == pathlib.PosixPath:
+    elif type(out_file) == pathlib.Path:
         extension = [out_file.suffix]
     else:
         raise ValueError('Output file format must either be string or pathlib.PosixPath')
