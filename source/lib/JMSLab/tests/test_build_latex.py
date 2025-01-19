9284f520c787a51c9f8538e290d0b95b76529097
Miika Päällysaho <miika.paallysaho@ne.su.se>
2025-01-19 22:47:54 +0200

    First pass at new unit test #119

--- source/lib/JMSLab/tests/test_build_latex.py
+++ source/lib/JMSLab/tests/test_build_latex.py
@@ -24,7 +24,7 @@ shutil_patch = mock.patch('%s.shutil.copy2' % path)
 TESTDIR = Path(__file__).resolve().parent
 os.chdir(TESTDIR)
 
-   
+
 class TestBuildLateX(unittest.TestCase):
 
     def setUp(self):
@@ -86,6 +86,31 @@ class TestBuildLateX(unittest.TestCase):
         for file in target:
             self.assertTrue(os.path.isfile(file))
 
+    @system_patch
+    def test_bibtex_multibib(self, mock_check_output):
+        """
+        Check that build_latex() works correctly when
+        the source '.tex' file contains multiple bibliographies.
+        """
+        mock_check_output.side_effect = fx.latex_side_effect
+        target = ['build/latex.pdf']
+        source = ['input/test_multibib.tex', 'input/test_ref.bib']
+
+        # Make sure check_bib works as expected
+        builder_attributes = {"name": "LaTeX", "valid_extensions": [".tex"]}
+        test_check_bib = LatexBuilder(target, source, {}, **builder_attributes)
+        test_check_bib.check_bib(source)
+        self.assertTrue(test_check_bib.checked_bib)
+
+        helpers.standard_test(self, build_latex, 'tex',
+                              system_mock = mock_check_output,
+                              source      = source,
+                              target      = target,
+                              nsyscalls   = 4)
+
+        for file in target:
+            self.assertTrue(os.path.isfile(file))
+
     def test_bad_extension(self):
         '''Test that build_latex() recognises an improper file extension'''
         helpers.bad_extension(self, build_latex, good = 'test.tex')
@@ -130,27 +155,25 @@ class TestBuildLateX(unittest.TestCase):
         with self.assertRaises(TypeError):
             build_latex('nonexistent_directory/latex.pdf',
                         ['input/latex_test_file.tex'], env = True)
-            
+
     def test_handout_nonunique_target(self):
         with self.assertRaises(ValueError):
             build_latex(target = ['path_to_clean.pdf', 'path_to_clean.pdf'], 
                         source = ['input/lyx_test_file.tex'],
                         env = {'HANDOUT_SFIX': '_clean'})
-            
-    
+
     def test_handout_suffix_mismatch(self):
         with self.assertRaises(ValueError):
             build_latex(target = ['path_to_clean.pdf', 'path_to_handout.pdf'], 
                         source = ['input/lyx_test_file.tex'],
                         env = {'HANDOUT_SFIX': '_'})
-            
-    
+
     def test_handout_missing_target(self):
         with self.assertRaises(ValueError):
             build_latex(target = ['path_to_clean.pdf'],
                         source = ['input/lyx_test_file.tex'],
                         env = {'HANDOUT_SFIX': '_'})
-            
+
     @subprocess_patch
     def test_handout_option(self, mock_check_output):
         '''
@@ -160,49 +183,48 @@ class TestBuildLateX(unittest.TestCase):
         with shutil_patch as mock_shutil:
             mock_shutil.side_effect = fx.shutil_copy2_effect
             mock_check_output.side_effect = fx.latex_side_effect
-            
+
             source = ['input/latex_test_file.tex']
             target = ['build/path_to_clean.pdf', 
                       'build/path_to_handout_.pdf',
                       'build/path_to_handout__.pdf']
-            
+
             helpers.standard_test(self, build_latex, 'tex',
                                   system_mock = mock_check_output,
                                   source      = source,
                                   target      = target,
                                   env         = {'HANDOUT_SFIX': '_'},
                                   nsyscalls   = 6)
-    
+
             self.assertTrue(os.path.isfile(target[0]))
             self.assertTrue(os.path.isfile(target[1]))
             self.assertTrue(os.path.isfile(target[2]))
-         
+
     @subprocess_patch
     def test_handout_default(self, mock_check_output):
-        
+
         with shutil_patch as mock_shutil:
             mock_shutil.side_effect = fx.shutil_copy2_effect
             mock_check_output.side_effect = fx.latex_side_effect
-    
+
             target = ['build/path_to_clean.pdf', 
                       'build/path_to_handout_.pdf',
                       'build/path_to_handout__.pdf']
             source = ['input/latex_test_file.tex']
-        
+
             helpers.standard_test(self, build_latex, 'latex',
                                   system_mock = mock_check_output,
                                   target = target,
                                   source = source, 
                                   nsyscalls = 6)
-        
+
             self.assertTrue(os.path.isfile(target[0]))
             self.assertTrue(os.path.isfile(target[1]))
             self.assertTrue(os.path.isfile(target[2]))
-            
-            
+
     def tearDown(self):
         shutil.rmtree(TESTDIR / 'build')
-        
+
 
 if __name__ == '__main__':
     unittest.main()
