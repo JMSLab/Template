f51a10f5a74bfd7c2b5bfc935ed0c955eb168ccc
Chris Liao <33707455+liaochris@users.noreply.github.com>
2026-03-25 10:27:50 -0400

    PR for #142: Correct issue with eps file closing (#143)

--- source/lib/JMSLab/remove_eps_info.py
+++ source/lib/JMSLab/remove_eps_info.py
@@ -1,7 +1,7 @@
-import re
 '''
 Stores program to remove dates from EPS files
 '''
+import re
 
 def remove_eps_info(infile):
     '''
@@ -11,21 +11,19 @@ def remove_eps_info(infile):
     Parameters
     ----------
     infile: str
-        Directory of the eps file 
+        Directory of the eps file
     '''
     to_remove = '%%CreationDate:'
     version_info = '%%Creator: Matplotlib'
-    eps_file = open(f'{infile}', 'r')
-    lines = eps_file.readlines()
-    eps_file.close
+    with open(infile, 'r') as eps_file:
+        lines = eps_file.readlines()
 
-    outfile = open(f'{infile}', 'w')
-    for line in lines:
-        if version_info in line:
-            # matplotlib version pattern
-            pattern = r'\s*\bv\S*(?=,\s*)'
-            new_attribution = re.sub(pattern, '', line)
-            outfile.write(new_attribution)
-        elif to_remove not in line:
-            outfile.write(line)
-    outfile.close()
+    with open(infile, 'w') as outfile:
+        for line in lines:
+            if version_info in line:
+                # matplotlib version pattern
+                pattern = r'\s*\bv\S*(?=,\s*)'
+                new_attribution = re.sub(pattern, '', line)
+                outfile.write(new_attribution)
+            elif to_remove not in line:
+                outfile.write(line)
