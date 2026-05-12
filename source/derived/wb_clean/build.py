d027bdb2fe462250b5a7081986f7adf4e441d396
simonessigaberg <95505173+simonessigaberg@users.noreply.github.com>
2026-05-12 10:28:49 -0400

    MWE for #159

--- source/derived/wb_clean/build.py
+++ source/derived/wb_clean/build.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 from source.lib.SaveData import SaveData
 from pathlib import Path
@@ -7,7 +8,10 @@ def Main():
     raw_dir = Path("datastore/raw/world_bank/orig")
     out_dir = Path("output/derived/wb_clean")
 
-    df = PrepareData(raw_dir)
+    with open("temp/derived/wb_clean/wb_clean.json") as f:
+        config = json.load(f)
+
+    df = PrepareData(raw_dir, config["nrows"])
     SaveData(
         df=df,
         keys=["Country Name"],
@@ -18,16 +22,16 @@ def Main():
     )
 
 
-def PrepareData(infolder):
+def PrepareData(infolder, nrows):
     gdp_df = pd.read_csv(
-        infolder / "API_NY.GDP.PCAP.CD_DS2_en_csv_v2_1740213.csv", header=2
+        infolder / "API_NY.GDP.PCAP.CD_DS2_en_csv_v2_1740213.csv", header=2, nrows=nrows
     )
 
     gdp_df = gdp_df[["Country Name", "2010"]]
     gdp_df.rename(columns={"2010": "GDP_2010"}, inplace=True)
 
     educ_df = pd.read_csv(
-        infolder / "API_SE.XPD.TOTL.GD.ZS_DS2_en_csv_v2_1740282.csv", header=2
+        infolder / "API_SE.XPD.TOTL.GD.ZS_DS2_en_csv_v2_1740282.csv", header=2, nrows=nrows
     )
 
     educ_df = educ_df[["Country Name", "2010"]]
